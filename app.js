a7e88a34174ae415639e15a133ff0214ad5ac2db
Robin Gottfried <robin.gottfried@fragaria.cz>
2019-08-02 16:04:37 +0200

    updates

--- app.js
+++ app.js
@@ -5,22 +5,30 @@ const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ.:+',
       common = " .;,!?-\n\m\t",
       latin_runes = latin + runes + common,
       runes_latin = runes + latin + common;
-      
+
+let text = '';
+
+if (location.hash.length) {
+  text = decodeURI(location.hash).substr(1);
+}      
+
 var app = new Vue({
   el: '#app',
   data: {
     translated: '',
-    text: ''
+    text: text
   },
+  created: function () {this.translate()},
   methods: {
       translate: function () {
-        this.text = this.text.toLocaleUpperCase().split('')
+        text = this.text.toLocaleUpperCase().split('')
             .map(char=>accents.indexOf(char)>=0 ? accentsInAscii[accents.indexOf(char)] : char)
             .join('');
         this.translated = this.text
             .split('')
             .map(char=>latin_runes[runes_latin.indexOf(char)])
             .join('');
+        this.text = text;
     	},
       swap: function () {
       	let tmp = this.text;
